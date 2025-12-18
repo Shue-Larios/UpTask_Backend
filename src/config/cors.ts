f255dddd85ea3f0635ee51ea553f7dc3e9e2968d
Shue_Larios <112102394+Shue-Larios@users.noreply.github.com>
2025-12-18 17:32:01 -0600

    three commit

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -6,11 +6,11 @@ export const corsConfig: CorsOptions = {
         const whitelist = [process.env.FRONTEND_URL]
         console.log(origin, "origen");
         console.log(whitelist, "whitelist");
-        // argv es como leer parametros que agregamos momento de correr el programa con npm run dev:api en package.json
-        if (process.argv[2] === '--api') {
-            // por que undefined xk thunderClient o postman envian el origen como undefined
-            whitelist.push(undefined)
-        }
+        // // argv es como leer parametros que agregamos momento de correr el programa con npm run dev:api en package.json
+        // if (process.argv[2] === '--api') {
+        //     // por que undefined xk thunderClient o postman envian el origen como undefined
+        //     whitelist.push(undefined)
+        // }
         // si la peticion se esta realizando de la lista blanca
         if (whitelist.includes(origin)) {
             // el primer campo es un error en este caso no hay error entoncs le decimos que si permitimos la conexion con el true
